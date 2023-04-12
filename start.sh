595ebcc35fce12bcd594f1f1fb0495945f31655a
bsefwe <92920899+bsefwe@users.noreply.github.com>
2023-04-12 07:17:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ wget $CADDYIndexPage -O usr/share/caddy/index.html && unzip -qo usr/share/caddy/
 
 
 # set config file
-cat etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
+
 cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > xray.json
 
 
