f471ad4fdb235c9946686001fed96832dee56ee0
bsefwe <92920899+bsefwe@users.noreply.github.com>
2023-04-18 04:19:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// const port = process.env.PORT || 8080;
+const server = "127.0.0.1";
 const port = 8080;
 const { execSync } = require("child_process");
 
-execSync(`chmod +x ./start.sh && ./start.sh`, {
-  cwd: "./",
-});
+execSync(`chmod +x ./start.sh && ./start.sh`);
